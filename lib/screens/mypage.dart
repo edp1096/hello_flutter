6864e94445f1ce7a43e8091b7add3934263c09b9
edp1096 <edp1096@naver.com>
2023-06-23 11:40:00 +0900

    text

--- lib/screens/mypage.dart
+++ lib/screens/mypage.dart
@@ -11,9 +11,14 @@ class _MyPageScreenState extends State<MyPageScreen> {
   @override
   Widget build(BuildContext context) {
     final scaffold = Container(
-      color: Colors.amber,
-      child: const Text("마이페이지 본문"),
-    );
+        color: Colors.amber,
+        child: const Column(
+          children: [
+            Text("마이페이지 본문"),
+            Text("안녕하세요."),
+            Text("반갑습니다."),
+          ],
+        ));
 
     return scaffold;
   }
