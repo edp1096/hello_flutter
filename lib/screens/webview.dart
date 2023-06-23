97b4e69ed8e1a8946c18de6ffa65e7a357bcd2d7
edp1096 <edp1096@naver.com>
2023-06-23 09:58:43 +0900

    vars

--- lib/screens/webview.dart
+++ lib/screens/webview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:hello_flutter/variables.dart';
 
 class WebViewScreen extends StatefulWidget {
   const WebViewScreen({Key? key}) : super(key: key);
@@ -7,27 +8,6 @@ class WebViewScreen extends StatefulWidget {
   @override
   State<WebViewScreen> createState() => _WebViewScreenState();
 }
-
-var controller = WebViewController()
-  ..setJavaScriptMode(JavaScriptMode.unrestricted)
-  ..setBackgroundColor(const Color(0x00000000))
-  ..setNavigationDelegate(
-    NavigationDelegate(
-      onProgress: (int progress) {
-        // Update loading bar.
-      },
-      onPageStarted: (String url) {},
-      onPageFinished: (String url) {},
-      onWebResourceError: (WebResourceError error) {},
-      onNavigationRequest: (NavigationRequest request) {
-        if (request.url.startsWith('about:blank')) {
-          return NavigationDecision.prevent;
-        }
-        return NavigationDecision.navigate;
-      },
-    ),
-  )
-  ..loadRequest(Uri.parse('https://www.naver.com'));
 
 class _WebViewScreenState extends State<WebViewScreen> {
   @override
