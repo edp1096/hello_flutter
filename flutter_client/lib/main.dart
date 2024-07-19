67e96b142fe6f91a27a2231ecebdcbf08c2326c1
edp1096 <edp1096@naver.com>
2024-07-19 21:44:50 +0900

    misc.

--- flutter_client/lib/main.dart
+++ flutter_client/lib/main.dart
@@ -1,40 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-// ignore: unused_import
-import 'package:permission_handler/permission_handler.dart';
 import 'container_page.dart';
 import 'landing_page.dart';
-
-// Future<void> _requestPermissions() async {
-//   // final statusSMS = await Permission.sms.request(); // request permission
-
-//   // if (statusSMS.isDenied) {
-//   //   if (await Permission.sms.request().isGranted) {
-//   //     debugPrint('SMS permission granted'); // permission granted
-//   //   } else {
-//   //     debugPrint('SMS permission denied'); // permission denied
-//   //   }
-//   // }
-
-//   // if (statusSMS.isPermanentlyDenied) {
-//   //   openAppSettings(); // permission denied
-//   // }
-
-//   final statusNOTI = await Permission.notification.request();
-
-//   if (statusNOTI.isDenied) {
-//     if (await Permission.notification.request().isGranted) {
-//       debugPrint('Notification permission granted');
-//     } else {
-//       debugPrint('Notification permission denied');
-//     }
-//   }
-
-//   // if (statusSMS.isPermanentlyDenied || statusNOTI.isPermanentlyDenied) {
-//   if (statusNOTI.isPermanentlyDenied) {
-//     openAppSettings();
-//   }
-// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -55,8 +22,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await _requestPermissions();
-  runApp(const MyApp());
-}
+void main() async => runApp(const MyApp());
